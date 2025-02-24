3c72fe9c7b92e7233359f498e9e52b9c0c38b476
JustaDebianDistro <gabekstenger@gmail.com>
2025-02-24 05:52:44 -0500

    emacs

---
+++ emacs/init.el
@@ -0,0 +1,9 @@
+(setq inhibit-startup-message t
+      visible-bell t)
+
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(menu-bar-mode 1)
+(global-display-line-numbers-mode 1)
+
+(load-theme 'modus-vivendi t)
