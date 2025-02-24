ada8496dfac0e2b2d12297b13eca61b21bcb0990
JustaDebianDistro <gabekstenger@gmail.com>
2025-02-24 08:40:59 -0500

    emacs packages

--- emacs/init.el
+++ emacs/init.el
@@ -7,3 +7,36 @@
 (global-display-line-numbers-mode 1)
 
 (load-theme 'modus-vivendi t)
+
+
+;; Enable package management
+(require 'package)
+(setq package-archives '(("gnu"   . "https://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
+(package-initialize)
+
+;; Bootstrap `use-package`
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; Enable `use-package`
+(eval-when-compile
+  (require 'use-package))
+
+(use-package zig-mode
+  :ensure t
+  :mode "\\.zig\\'"
+  :config
+  (setq zig-format-on-save t))
+
+(use-package lsp-mode
+  :ensure t
+  :hook (zig-mode . lsp)
+  :commands lsp)
+
+(use-package company
+  :ensure t
+  :config
+  (global-company-mode t))
+
