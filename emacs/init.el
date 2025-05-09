f2301e6fc84520d4c290c1d518fbc2ea8a42fe7f
JustaDebianDistro <thespacejamzdvd@gmail.com>
2025-05-09 06:18:09 -0400

    nix format and lsp for emacs

--- emacs/init.el
+++ emacs/init.el
@@ -1,3 +1,4 @@
+;; Basic UI settings
 (setq inhibit-startup-message t
       visible-bell t)
 
@@ -8,35 +9,64 @@
 
 (load-theme 'modus-vivendi t)
 
+;; Silence compiler warnings
+(setq byte-compile-warnings '(not unused-value))
 
-;; Enable package management
+;; Package management
 (require 'package)
 (setq package-archives '(("gnu"   . "https://elpa.gnu.org/packages/")
                          ("melpa" . "https://melpa.org/packages/")))
 (package-initialize)
 
-;; Bootstrap `use-package`
+;; Bootstrap use-package
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
-;; Enable `use-package`
 (eval-when-compile
   (require 'use-package))
 
+;; Zig configuration
 (use-package zig-mode
   :ensure t
   :mode "\\.zig\\'"
   :config
   (setq zig-format-on-save t))
 
+;; LSP configuration
 (use-package lsp-mode
   :ensure t
-  :hook (zig-mode . lsp)
-  :commands lsp)
+  :hook
+  ((zig-mode . lsp)
+   (nix-mode . lsp))
+  :config
+  (lsp-register-client
+   (make-lsp-client
+    :new-connection (lsp-stdio-connection '("nil"))
+    :activation-fn (lsp-activate-on "nix")
+    :server-id 'nil
+    :priority 1
+    :download-server-fn
+    (lambda (_client callback error-callback _update?)
+      (if-let ((nil-path (executable-find "nil")))
+          (funcall callback)
+        (funcall error-callback "Could not find 'nil' in PATH"))))))
 
+;; Company mode
 (use-package company
   :ensure t
   :config
   (global-company-mode t))
 
+;; Nix mode
+(use-package nix-mode
+  :ensure t
+  :mode "\\.nix\\'")
+
+;; Formatting
+(use-package format-all
+  :ensure t
+  :hook (nix-mode . format-all-mode)
+  :config
+  (add-to-list 'format-all-formatters '("Nix" nixpkgs-fmt))
+  (setq byte-compile-warnings '(not docstrings)))
