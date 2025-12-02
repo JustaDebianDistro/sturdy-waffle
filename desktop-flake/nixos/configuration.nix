59c234193f4f266fee0043f622cd5ac629271546
JustaDebianDistro <thespacejamzdvd@gmail.com>
2025-12-02 06:03:48 -0500

    clean

--- desktop-flake/nixos/configuration.nix
+++ desktop-flake/nixos/configuration.nix
@@ -17,41 +17,24 @@
     ./hardware-configuration.nix
   ];
 
-#  nixpkgs = {
-#    overlays = [
-      # If you want to use overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
+#  nix = let
+#    flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
+#  in {
+#    settings = {
+#      experimental-features = "nix-command flakes";
+#      flake-registry = "";
+#      # Workaround for https://github.com/NixOS/nix/issues/9574
+#      nix-path = config.nix.nixPath;
+#    };
+#    # Opinionated: disable channels
+#    channel.enable = false;
+#
+#    # Opinionated: make flake registry and nix path match flake inputs
+#    registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
+#    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+  #  };
 
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
- #   ];
-    # Configure your nixpkgs instance
-   # config = {
-      # Disable if you don't want unfree packages
-     # allowUnfree = true;
-   # };
- # };
-
-  nix = let
-    flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
-  in {
-    settings = {
-      experimental-features = "nix-command flakes";
-      flake-registry = "";
-      # Workaround for https://github.com/NixOS/nix/issues/9574
-      nix-path = config.nix.nixPath;
-    };
-    # Opinionated: disable channels
-    channel.enable = false;
-
-    # Opinionated: make flake registry and nix path match flake inputs
-    registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
-    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
-  };
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
@@ -121,5 +104,5 @@
   };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  system.stateVersion = "24.05";
+  system.stateVersion = "24.11";
 }
