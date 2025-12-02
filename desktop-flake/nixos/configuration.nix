118f542a60afa3af2012e330be53df6e4814d31b
JustaDebianDistro <thespacejamzdvd@gmail.com>
2025-12-02 05:30:39 -0500

    fix fix fix

--- desktop-flake/nixos/configuration.nix
+++ desktop-flake/nixos/configuration.nix
@@ -11,21 +11,14 @@
 }: {
   # You can import other NixOS modules here
   imports = [
-    # If you want to use modules from other flakes (such as nixos-hardware):
-    # inputs.hardware.nixosModules.common-cpu-amd
-    # inputs.hardware.nixosModules.common-ssd
-
-    # You can also split up your configuration and import pieces of it here:
     ./users.nix
     ./pkgs.nix
 
-    # Import your generated (nixos-generate-config) hardware configuration
     ./hardware-configuration.nix
   ];
 
-  nixpkgs = {
-    # You can add overlays here
-    overlays = [
+#  nixpkgs = {
+#    overlays = [
       # If you want to use overlays exported from other flakes:
       # neovim-nightly-overlay.overlays.default
 
@@ -35,21 +28,19 @@
       #     patches = [ ./change-hello-to-hi.patch ];
       #   });
       # })
-    ];
+ #   ];
     # Configure your nixpkgs instance
    # config = {
       # Disable if you don't want unfree packages
      # allowUnfree = true;
    # };
-  };
+ # };
 
   nix = let
     flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
   in {
     settings = {
-      # Enable flakes and new 'nix' command
       experimental-features = "nix-command flakes";
-      # Opinionated: disable global registry
       flake-registry = "";
       # Workaround for https://github.com/NixOS/nix/issues/9574
       nix-path = config.nix.nixPath;
@@ -62,17 +53,14 @@
     nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
   };
 
-  # FIXME: Add the rest of your current configuration
-
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   
   # TODO: Set your hostname
-  networking.hostName = "UNSC-Dresden"; # Define your hostname.
-  #networking.wireless.enable = true;
+  networking.hostName = "UNSC-Midnightlamp";
 
-    # Enable networking
+  # Enable networking
   networking.networkmanager.enable = true;
 
   # Set your time zone.
@@ -92,6 +80,16 @@
     LC_TELEPHONE = "en_US.UTF-8";
     LC_TIME = "en_US.UTF-8";
   };
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver.enable = true;
+  services.xserver.videoDrivers = [ "amdgpu" ];
 
   networking.nameservers = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
 
@@ -121,59 +119,6 @@
     alsa.support32Bit = true;
     pulse.enable = true;
   };
-
-#  nixpkgs.config.permittedInsecurePackages = [
-#    "dotnet-runtime-7.0.20"
-#    "freeimage-3.18.0-unstable-2024-04-18"
-#    "ventoy-1.1.05"
-#  ];
-
-#  services.flatpak.enable = true;
-#  programs.firefox.enable = true;
-#  programs.steam = {
-#   enable = true;
-#   remotePlay.openFirewall = true; # Open ports in the firewall for Steam# Remote Play
-#   dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server#
-#   localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-# };
-
-#  # Enable the KDE Plasma Desktop Environment.
-#  services.displayManager.sddm.enable = true;
-#  services.desktopManager.plasma6.enable = true;
-#  services.desktopManager.cosmic.enable = true;
-#  programs.hyprland.enable = true;
-#  services.mullvad-vpn.enable = true;
-#
-
- # # TODO: Configure your system-wide user settings (groups, etc), add more users as needed.
- # users.users = {
- #   # FIXME: Replace with your username
- #   your-username = {
- #     # TODO: You can set an initial password for your user.
- #     # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
- #     # Be sure to change it (using passwd) after rebooting!
- #     initialPassword = "correcthorsebatterystaple";
- #     isNormalUser = true;
-##      openssh.authorizedKeys.keys = [
- #       # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
- #     ];
- #     # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
- #     extraGroups = ["wheel"];
- #   };
- # };
-
-#  # This setups a SSH server. Very important if you're setting up a headless system.
-#  # Feel free to remove if you don't need it.
-#  services.openssh = {
-#    enable = true;
-#    settings = {
-#      # Opinionated: forbid root login through SSH.
-#      PermitRootLogin = "no";
-#      # Opinionated: use keys only.
-#      # Remove if you want to SSH using passwords
-#      PasswordAuthentication = false;
-#    };
-#  };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "24.05";
--- laptop-flake/nixos/configuration.nix
+++ laptop-flake/nixos/configuration.nix
@@ -122,59 +122,6 @@
     pulse.enable = true;
   };
 
-#  nixpkgs.config.permittedInsecurePackages = [
-#    "dotnet-runtime-7.0.20"
-#    "freeimage-3.18.0-unstable-2024-04-18"
-#    "ventoy-1.1.05"
-#  ];
-
-#  services.flatpak.enable = true;
-#  programs.firefox.enable = true;
-#  programs.steam = {
-#   enable = true;
-#   remotePlay.openFirewall = true; # Open ports in the firewall for Steam# Remote Play
-#   dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server#
-#   localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-# };
-
-#  # Enable the KDE Plasma Desktop Environment.
-#  services.displayManager.sddm.enable = true;
-#  services.desktopManager.plasma6.enable = true;
-#  services.desktopManager.cosmic.enable = true;
-#  programs.hyprland.enable = true;
-#  services.mullvad-vpn.enable = true;
-#
-
- # # TODO: Configure your system-wide user settings (groups, etc), add more users as needed.
- # users.users = {
- #   # FIXME: Replace with your username
- #   your-username = {
- #     # TODO: You can set an initial password for your user.
- #     # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
- #     # Be sure to change it (using passwd) after rebooting!
- #     initialPassword = "correcthorsebatterystaple";
- #     isNormalUser = true;
-##      openssh.authorizedKeys.keys = [
- #       # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
- #     ];
- #     # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
- #     extraGroups = ["wheel"];
- #   };
- # };
-
-#  # This setups a SSH server. Very important if you're setting up a headless system.
-#  # Feel free to remove if you don't need it.
-#  services.openssh = {
-#    enable = true;
-#    settings = {
-#      # Opinionated: forbid root login through SSH.
-#      PermitRootLogin = "no";
-#      # Opinionated: use keys only.
-#      # Remove if you want to SSH using passwords
-#      PasswordAuthentication = false;
-#    };
-#  };
-
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "24.05";
 }
