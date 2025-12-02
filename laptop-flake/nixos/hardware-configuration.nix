735ec632da750aa6a053e1fd5db52a7661a21ff0
JustaDebianDistro <thespacejamzdvd@gmail.com>
2025-12-02 01:38:23 -0500

    fix

--- laptop-flake/nixos/hardware-configuration.nix
+++ laptop-flake/nixos/hardware-configuration.nix
@@ -36,15 +36,4 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-}# This is just an example, you should generate yours with nixos-generate-config and put it in here.
-{
-  boot.loader.systemd-boot.enable = true;
-
-  fileSystems."/" = {
-    device = "/dev/sda1";
-    fsType = "ext4";
-  };
-
-  # Set your system kind (needed for flakes)
-  nixpkgs.hostPlatform = "x86_64-linux";
 }
