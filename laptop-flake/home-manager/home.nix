bf79e471e0f91cc8ccb322854fd5fece6cbc99de
JustaDebianDistro <thespacejamzdvd@gmail.com>
2025-12-05 05:02:43 -0500

    stuff3

--- laptop-flake/home-manager/home.nix
+++ laptop-flake/home-manager/home.nix
@@ -34,7 +34,7 @@
       # Disable if you don't want unfree packages
       allowUnfree = true;
       # Workaround for https://github.com/nix-community/home-manager/issues/2942
-      allowUnfreePredicate = _: true;
+    #  allowUnfreePredicate = _: true;
     };
   };
 
@@ -46,7 +46,7 @@
 
   # Add stuff for your user as you see fit:
   # programs.neovim.enable = true;
-  # home.packages = with pkgs; [ steam ];
+   home.packages = with pkgs; [ firefox ];
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
